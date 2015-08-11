8e94cc40a4db8237c19702be28cf6f8e281480c9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-08-11 20:56:40 +0200

    Use diminish-modeas backend

--- modeline-pack.el
+++ modeline-pack.el
@@ -4,50 +4,54 @@
 
 ;;; Code:
 
+(require 'dash)
+(require 's)
+(require 'diminish)
+
 (defvar mode-line-cleaner-alist
-  `((auto-complete-mode       . " α")
-    (yas-minor-mode           . " γ")
-    (paredit-mode             . " Φ")
-    (eldoc-mode               . "")
-    (abbrev-mode              . "")
-    (undo-tree-mode           . " τ")
-    (volatile-highlights-mode . " υ")
-    (elisp-slime-nav-mode     . " δ")
-    (nrepl-mode               . " ηζ")
-    (nrepl-interaction-mode   . " ηζ")
-    (cider-mode               . " ηζ")
-    (cider-interaction        . " ηζ")
+  `((auto-complete-mode       " α")
+    (yas-minor-mode           " γ")
+    (paredit-mode             " Φ")
+    (eldoc-mode               "")
+    (abbrev-mode              "")
+    (guide-key-mode           "")
+    (guru-mode                "")
+    (undo-tree-mode           " τ")
+    (volatile-highlights-mode " υ")
+    (elisp-slime-nav-mode     " δ")
+    (nrepl-mode               " ηζ")
+    (nrepl-interaction-mode   " ηζ")
+    (cider-mode               " ηζ")
+    (cider-interaction        " ηζ")
     ;; Major modes
-    (clojure-mode             . "cλ")
-    (hi-lock-mode             . "")
-    (python-mode              . "pλ")
-    (emacs-lisp-mode          . "eλ")
-    (markdown-mode            . "md")
-    (magit                    . "ma")
-    (haskell-mode             . "hλa")
-    (tuareg-mode              . "mλ")
-    (flymake-mode             . "fm")
-    (js2-mode                 . "jλ"))
-  "Alist for `clean-mode-line'.
+    (clojure-mode             "cλ")
+    (hi-lock-mode             "")
+    (python-mode              "pλ")
+    (emacs-lisp-mode          "eλ2")
+    (markdown-mode            "md")
+    (magit                    "ma")
+    (haskell-mode             "hλa")
+    (tuareg-mode              "mλ")
+    (flymake-mode             "fm")
+    (js2-mode                 "jλ")
+    (company-mode             " Ψ"))
+  "Alist for `modeline-pack-clean-mode-line'.
 
 When you add a new element to the alist, keep in mind that you
 must pass the correct minor/major mode symbol and a string you
 want to use in the modeline *in lieu of* the original.")
 
-(defun clean-mode-line ()
+(defun modeline-pack-clean-mode-line ()
   "Pretty symbol for the modeline."
   (interactive)
-  (loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                  (mode-str (cdr cleaner))
-                  (old-mode-str (cdr (assq mode minor-mode-alist))))
-             (when old-mode-str
-               (setcar old-mode-str mode-str))
-             ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
+  (loop for mmode in mode-line-cleaner-alist
+        do (-let (((mode mode-str) mmode))
+             (if (eq mode major-mode)
+                 (setq mode-name (s-trim mode-str))
+               (when (assq mode minor-mode-alist)
+                 (diminish mode (s-trim-right mode-str)))))))
 
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+(modeline-pack-clean-mode-line)
 
 ;;; Greek letters - C-u C-\ greek ;; C-\ to revert to default
 ;;; ς ε ρ τ υ θ ι ο π α σ δ φ γ η ξ κ λ ζ χ ψ ω β ν μ
